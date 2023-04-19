e751b2668f491fb0ce17bd9cf8abf794302fd9c4
Mirage Concorde <mirage.concorde@googlemail.com>
2023-04-19 20:03:45 +0200

    sort
    sort

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 
 
-
+use inline_python::python;
 fn main() {
     println!("Hello, world!");
-    
+    //let a  = (4,5,6);
+    python! {
+        print("ii")
+
+}
+
+
+let mut v = vec![3, 4, 5];
+
+println!("init");
+for i in 0..100000000{
+   v.push(i%337);
+};
+
+println!("endinit");
+println!("start");
+v.sort_unstable();
+println!("end");
+
 }
